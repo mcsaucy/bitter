81c146b28efe5eed3987574429e72d3d16c4972b
Josh McSavaney <me@mcsau.cc>
2024-05-18 14:08:56 -0400

    add ForEach

--- bitter.go
+++ bitter.go
@@ -12,7 +12,7 @@ type Iterator[K, V any] struct {
 
 func FromSlice[V any](s []V) Iterator[int, V] {
 	return Iterator[int, V]{
-			s: func(yield func(int, V) bool) {
+		s: func(yield func(int, V) bool) {
 			for i, v := range s {
 				if !yield(i, v) {
 					return
@@ -25,4 +25,25 @@ func FromSlice[V any](s []V) Iterator[int, V] {
 // All exposes an iter.Seq for use with range or iter.Pull.
 func (i Iterator[K, V]) All() iter.Seq2[K, V] {
 	return i.s
+}
+
+// ForEach executes "do" for each entry of "in".
+func ForEach[K, V, Kp, Vp any](in Iterator[K, V], do func(K, V) (Kp, Vp)) Iterator[Kp, Vp] {
+	return Iterator[Kp, Vp]{
+		s: func(yield func(Kp, Vp) bool) {
+			for k, v := range in.s {
+				kp, vp := do(k, v)
+				if !yield(kp, vp) {
+					return
+				}
+			}
+		},
+	}
+}
+
+// ForEachV is like ForEach, except it only processes the right value (V) and preserves the left value (K).
+func ForEachV[K, V, Vp any](in Iterator[K, V], do func(V) Vp) Iterator[K, Vp] {
+	return ForEach(in, func(k K, v V) (K, Vp) {
+		return k, do(v)
+	})
 }
--- bitter_test.go
+++ bitter_test.go
@@ -1,6 +1,7 @@
 package bitter
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -17,5 +18,39 @@ func TestAll(t *testing.T) {
 	if !cmp.Equal(want, have) {
 		t.Errorf("FromSlice(%q).All() --> %q, want %q", want, have, want)
 	}
-	
+
+}
+
+func TestForEach(t *testing.T) {
+	in := []string{"aperol", "campari", "fernet", "cynar"}
+	want := []string{"APEROL", "CAMPARI", "FERNET", "CYNAR"}
+
+	have := make([]string, len(in))
+	doUpper := func(i int, v string) (int, string) {
+		return i, strings.ToUpper(v)
+	}
+
+	for i, v := range ForEach(FromSlice(want), doUpper).All() {
+		have[i] = v
+	}
+
+	if !cmp.Equal(want, have) {
+		t.Errorf("ForEach(FromSlice(%q), doUpper).All() --> %q, want %q", in, have, want)
+	}
+
+}
+
+func TestForEachV(t *testing.T) {
+	in := []string{"aperol", "campari", "fernet", "cynar"}
+	want := []string{"APEROL", "CAMPARI", "FERNET", "CYNAR"}
+
+	have := make([]string, len(in))
+	for i, v := range ForEachV(FromSlice(want), strings.ToUpper).All() {
+		have[i] = v
+	}
+
+	if !cmp.Equal(want, have) {
+		t.Errorf("ForEachV(FromSlice(%q), strings.ToUpper).All() --> %q, want %q", in, have, want)
+	}
+
 }
